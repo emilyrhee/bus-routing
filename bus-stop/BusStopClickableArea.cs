761c7de190b30db0b2e30a3e0861c7f41a3ad750
emilyrhee <emilyrhee99@gmail.com>
2025-11-09 15:04:58 -0500

    feat: clicking on a bus stop will make a line that follows cursor

--- bus-stop/BusStopClickableArea.cs
+++ bus-stop/BusStopClickableArea.cs
@@ -4,6 +4,25 @@ using System;
 public partial class BusStopClickableArea : Area2D
 {
     private int test = 0;
+    private Node2D _currentLevel;
+    private Line2D _previewLine;
+
+    public override void _Ready()
+    {
+        _currentLevel = GetTree().CurrentScene as Node2D ?? GetParent() as Node2D;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (_previewLine == null)
+            return;
+
+        if (_previewLine.GetPointCount() < 2)
+            _previewLine.AddPoint(GetGlobalMousePosition());
+        else
+            _previewLine.SetPointPosition(1, GetGlobalMousePosition());
+    }
+
     private void _on_input_event(Node viewport, InputEvent @event, long shapeIdx)
     {
         if (!@event.IsLeftMouseClick())
@@ -17,6 +36,10 @@ public partial class BusStopClickableArea : Area2D
             {
                 GD.Print(node);
             }
+
+            _previewLine = new Line2D();
+            _previewLine.AddPoint(GetGlobalMousePosition());
+            _currentLevel.AddChild(_previewLine);
         }
     }
 }
