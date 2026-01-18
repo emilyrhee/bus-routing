3a909b3d9eae38c5a94c4f8533241ffda0d6f08f
emilyrhee <emilyrhee99@gmail.com>
2026-01-18 22:00:58 +0000

    style: make lighten factor higher for destinations
    destinations naturally look darker due to the way the normal maps for the building sprites are

--- bus-stop/detector/BusStopDetector.cs
+++ bus-stop/detector/BusStopDetector.cs
@@ -7,10 +7,24 @@ using System;
 /// </summary>
 public partial class BusStopDetector : RayCast2D
 {
+    private Node _parentNode;
     private Sprite2D _buildingSprite;
     private Color _originalColor;
     private Color _targetColor;
-    private readonly Color _lightenFactor = new(1.4f, 1.4f, 1.4f, 1.0f);
+    private readonly Color _houseLightenFactor = new(1.4f, 1.4f, 1.4f, 1.0f);
+    /// <summary>
+    /// The lighten factor is higher for destination buildings because the
+    /// sprites look darker due to how the normal maps are set up.
+    /// </summary>
+    private readonly Color _destinationLightenFactor = new
+    (
+        1.7f, 1.7f, 1.7f, 1.0f
+    );
+    /// <summary>
+    /// The current lighten factor being applied. Depends on whether the parent
+    /// node is a destination or a house.
+    /// </summary>
+    private Color _lightenFactor;
     private float _lerpSpeed = 10f;
 
     /// <summary>
@@ -37,14 +51,22 @@ public partial class BusStopDetector : RayCast2D
     {
         _buildingSprite = GetParent<Node2D>().GetNode<Sprite2D>("Sprite2D");
         _originalColor = _buildingSprite.Modulate;
+        _parentNode = GetParent();
     }
 
     public override void _PhysicsProcess(double delta)
     {
         if (ReachableBusStop != null)
+        {
+            _lightenFactor = _parentNode is Destination ? _destinationLightenFactor : _houseLightenFactor;
             _targetColor = _originalColor * _lightenFactor;
+        }
         else
             _targetColor = _originalColor;
-        _buildingSprite.Modulate = _buildingSprite.Modulate.Lerp(_targetColor, (float)(delta * _lerpSpeed));
+
+        _buildingSprite.Modulate = _buildingSprite.Modulate.Lerp
+        (
+            _targetColor, (float)(delta * _lerpSpeed)
+        );
     }
 }
